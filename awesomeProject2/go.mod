1b1ce171ee3f9cd8e0edcf40ff85bc2f2d1c9f31
pp221B030105 <aytzhanr@list.ru>
2023-02-01 11:12:01 +0600

    some massage

--- awesomeProject2/go.mod
+++ awesomeProject2/go.mod
@@ -1,3 +1,5 @@
 module github.com/Ramazan21B/Golang/awesomeProject2
 
 go 1.19
+
+require github.com/Ramazan21B/Golang/awesomeProject1 v0.0.0-20230201045233-cc051348d65d // indirect
--- awesomeProject2/main.go
+++ awesomeProject2/main.go
@@ -1,5 +1,16 @@
-package awesomeProject2
+package main
+
+import (
+	"fmt"
+	"github.com/Ramazan21B/Golang/awesomeProject1/pkg"
+)
 
 func main() {
+	pkg.Hello()
+	fmt.Println("oweefniwefnoifnweoiefnwefoi")
 
 }
+
+//git add .
+//git commit -m"some massage"
+//git push origin main
